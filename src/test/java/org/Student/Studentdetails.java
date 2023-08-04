968fe27b119ec198acdf98e65641281b99270724
dhaya1407 <dhayanithi1407@gmail.com>
2023-08-04 13:34:29 +0530

    change

---
+++ src/test/java/org/Student/Studentdetails.java
@@ -0,0 +1,63 @@
+package org.Student;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Studentdetails {
+	
+	
+	private void m1() {
+		System.out.println("print1");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Studentdetails s = new Studentdetails();
+		s.m1();
+		
+		
+	}
+		
+		
+			
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+
